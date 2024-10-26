ee769060bf6bc197c8038d0b9fcd38f3a41d1c6b
KsyKsy-dev <ksy.codedev@gmail.com>
2024-10-27 01:57:33 +0200

    cl min ni

--- main.js
+++ main.js
@@ -3,22 +3,34 @@
 // first step :get number from 2 to n;
 // second step: check each number if it is prime
 
-function checkUpTo_n(n) {
-  for (let i = 2; i <= n; i++) {
-    if (IsPrime(i)) {
-      console.log(i, "N is not prime");
-    } else {
-      console.log("is prime ", i);
-    }
-  }
-}
+// function checkUpTo_n(n) {
+//   for (let i = 2; i <= n; i++) {
+//     if (IsPrime(i)) {
+//       console.log(i, "N is not prime");
+//     } else {
+//       console.log("is prime ", i);
+//     }
+//   }
+// }
 
-function IsPrime(number) {
-  for (let i = 2; i < number; i++) {
-    if (number % i == 0) {
-      console.log(i);
-      return false;
-    } else return true;
-  }
-}
-checkUpTo_n(7);
+// function IsPrime(number) {
+//   for (let i = 2; i < number; i++) {
+//     if (number % i == 0) {
+//       console.log(i);
+//       return false;
+//     } else return true;
+//   }
+// }
+// checkUpTo_n(7);
+
+// check age options
+// function checkAge(age) {
+//   return age > 18 || confirm(" did you get permition of your parrents?");
+// }
+// checkAge(17);
+
+// min number
+// function min(a, b) {
+//   return a > b ? b : a;
+// }
+// console.log(min(1, 2));
