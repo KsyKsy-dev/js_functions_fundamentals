dbcbc3dd4f6626755af5413c9eb7f740a43f8c00
KsyKsy-dev <ksy.codedev@gmail.com>
2024-10-26 23:11:45 +0200

    rewrite function

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 "use strct";
 //ptime number
-
-function checkIfIsPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-      console.log(" not ptime", i, n);
-    } else {
-      console.log("is prime", i, n);
-    }
-  }
-}
-checkIfIsPrime(7);
