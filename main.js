cf540e624c3286e239f69b4b054eeb82d54bb0cf
KsyKsy-dev <ksy.codedev@gmail.com>
2024-10-26 23:26:20 +0200

    check number functin

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-"use strct";
+"use strict";
 //ptime number
+function checkIfNumbrerIsPtrime(n) {
+  // first step: get number second step: loop it to the number from 2 to n to check if it's prime
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      console.log(i, n, " is prime");
+    } else {
+      console.log(i, n, " isn't prime");
+    }
+  }
+}
+checkIfNumbrerIsPtrime(4);
