eb797415c82515fd165daaaf98cf4351bc3f9058
KsyKsy-dev <ksy.codedev@gmail.com>
2024-12-14 22:21:25 +0100

    emitter trigger

--- node.js
+++ node.js
@@ -1,2 +1,6 @@
-const EventEmitter = requier("events");
-console.log(EventEmitter);
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+emitter.on("event name", (arg) => {
+  console.log(arg);
+});
+emitter.emit("event name", { id: 1 });
