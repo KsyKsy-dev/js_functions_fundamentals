c442347b0b1450f8288314a3aa470a14af69c40e
KsyKsy-dev <ksy.codedev@gmail.com>
2024-12-14 22:10:52 +0100

    EventEmitter class

--- node.js
+++ node.js
@@ -1,7 +1,2 @@
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
-emitter.on("event", function (eventArg) {
-  console.log("Message is logged!", eventArg);
-});
-
-emitter.emit("event", { id: 1, name: "Jony" });
+const EventEmitter = requier("events");
+console.log(EventEmitter);
