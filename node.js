7f7f93ae18b662a72268a6b239183459da613043
KsyKsy-dev <ksy.codedev@gmail.com>
2024-12-14 01:26:06 +0100

    Event's argument

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
-emitter.on("event", function () {
-  console.log("Message is logged!");
+emitter.on("event", function (eventArg) {
+  console.log("Message is logged!", eventArg);
 });
-emitter.emit("event");
+
+emitter.emit("event", { id: 1, name: "Jony" });
