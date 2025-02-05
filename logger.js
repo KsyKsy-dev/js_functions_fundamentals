2534286bce6732817afab61da8d24b0562a7eb54
KsyKsy-dev <ksy.codedev@gmail.com>
2025-02-05 01:02:22 +0100

    Node eef arguments

--- logger.js
+++ logger.js
@@ -1,3 +1,2 @@
 const logger = require("./node");
 console.log(logger);
-logger.log("message");
--- node.js
+++ node.js
@@ -2,7 +2,6 @@ let url = "http//www.swi.org";
 function log(m) {
   console.log(m);
 }
-
-module.exports.log = log;
-
-console.log(module);
+console.log(__dirname);
+console.log(__filename);
+module.exports = log;
