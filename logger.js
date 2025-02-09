52d2a77564a803cf75f22cf811f8b3c574bf4d64
KsyKsy-dev <ksy.codedev@gmail.com>
2025-02-09 01:59:20 +0100

    Node Fs ASync

--- logger.js
+++ logger.js
@@ -2,3 +2,7 @@ const fs = require("fs");
 let files = fs.readdirSync("./");
 
 console.log(files);
+fs.readdir("./", function (err, files) {
+  if (err) console.log(err);
+  else console.log(files);
+});
