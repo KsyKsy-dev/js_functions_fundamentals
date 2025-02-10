6caf6f1171dbc3a0216b1daa4eb9013e1c49a748
KsyKsy-dev <ksy.codedev@gmail.com>
2025-02-10 05:18:33 +0100

    EventEmitter function arg

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 const EventEmitter = require("events");
+console.log(typeof EventEmitter);
 const emmiter = new EventEmitter();
-emmiter.on("myEvent", function () {
-  console.log("This event is the fire");
+emmiter.on("myEvent", function (eventArg) {
+  console.log("This event is the fire ", eventArg);
 });
-emmiter.emit("myEvent");
+emmiter.emit("myEvent", { id: "user", url: "http//www..." });
