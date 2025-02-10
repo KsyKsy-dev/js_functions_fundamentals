310b2c74e756967a10252da984803261fddf7962
KsyKsy-dev <ksy.codedev@gmail.com>
2025-02-10 05:20:28 +0100

    EventEmmiter arrow functiion

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 const EventEmitter = require("events");
 console.log(typeof EventEmitter);
 const emmiter = new EventEmitter();
-emmiter.on("myEvent", function (eventArg) {
+emmiter.on("myEvent", (eventArg) => {
   console.log("This event is the fire ", eventArg);
 });
 emmiter.emit("myEvent", { id: "user", url: "http//www..." });
