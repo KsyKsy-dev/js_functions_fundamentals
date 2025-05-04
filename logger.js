cb93cb8ae33106eae5cd72cb9c930e4a8b515789
KsyKsy-dev <ksy.codedev@gmail.com>
2025-05-04 19:31:27 +0200

    add node module

--- logger.js
+++ logger.js
@@ -1,6 +1,1 @@
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
-emitter.on("Event", (arg) => {
-  console.log("let's ", arg, " to be!");
-});
-emitter.emit("Event", "the action!!!");
+
--- node.js
+++ node.js
@@ -1,7 +1,1 @@
-let url = "http//www.swi.org";
-function log(m) {
-  console.log(m);
-}
-console.log(__dirname);
-console.log(__filename);
-module.exports = log;
+console.log(module);
