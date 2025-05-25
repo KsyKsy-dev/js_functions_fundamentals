6bcb3c46f60cd40eb61cdf5410c9b4ad7b86d9c0
KsyKsy-dev <ksy.codedev@gmail.com>
2025-05-25 17:53:10 +0200

    mìnode event emitter arg

--- logger.js
+++ logger.js
@@ -2,6 +2,8 @@ let EventEmitter = require("events");
 
 let emitter = new EventEmitter();
 // Register event
-emitter.on("Event name", " Event is rised");
+emitter.on("Event name", function (arg) {
+  console.log(" Event is rised: ", arg);
+});
 // Rise an event
-emitter.emit("Event name");
+emitter.emit("Event name", { url: "http://www.sist.orc", permission: 1 });
