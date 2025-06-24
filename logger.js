3b35997857fd5f5ab1bdf4d279b8d133f83da627
KsyKsy-dev <ksy.codedev@gmail.com>
2025-06-24 11:50:45 +0200

    Class extenss

--- logger.js
+++ logger.js
@@ -7,3 +7,15 @@ function log(message) {
   emitter.emit("My event", prop);
 }
 module.exports = log;
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  describe() {
+    console.log(`Im ${this.name} and I am ${this.age} old`);
+  }
+}
+let person1 = new Person("Lia", 45);
+console.log(person1);
