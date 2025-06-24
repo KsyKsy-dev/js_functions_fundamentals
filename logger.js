d7c37b2e2d4f7c72d795f2a55f6f8a1b863f27ce
KsyKsy-dev <ksy.codedev@gmail.com>
2025-06-24 13:04:37 +0200

    Event emmiter exporst

--- logger.js
+++ logger.js
@@ -1,43 +1,8 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
-
 function log(message) {
-  console.log("User is ", message);
-
-  emitter.emit("My event", prop);
+  console.log(message);
+  //rise an event
+  emitter.emit("evName", "arg");
 }
 module.exports = log;
-
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  describe() {
-    console.log(`Im ${this.name} and I am ${this.age} old`);
-  }
-}
-let person1 = new Person("Lia", 45);
-console.log(person1);
-
-class Dev extends Person {
-  constructor(name, age, experience) {
-    super(name, age);
-    // castomprop
-    this.experience = experience;
-  }
-  code() {
-    console.log(`${this.name} is coding for ${this.experience} of yesrs`);
-  }
-}
-let dev1 = new Dev("Ann", 46, 6);
-const dev2 = new Dev("Jane", 33, 12);
-dev2.code();
-const devs = [new Dev("Jacky", 45, 10), new Dev("Lucia", 43, 18)];
-
-function printDevs(programmers) {
-  for (let programmer of programmers) {
-    programmer.code();
-  }
-}
-printDevs(devs);
--- node.js
+++ node.js
@@ -1,33 +1,8 @@
-// 1 create class prod
-// 2 create metod print cl prodact
-// 3 create method calc taxes to print prduct price with  taxes
-
-// class Product {
-//   constructor(productType, price) {
-//     this.productType = productType;
-//     this.price = price;
-//   }
-
-//   printProductPropertyes() {
-//     console.log(`Product ${this.productType}`);
-//     console.log(`Price ${this.price}`);
-//   }
-//   calcProductTaxes(salesTax) {
-//     return this.price + this.price * salesTax;
-//   }
-// }
-// const salesTax = 0.05;
-// const prod1 = new Product("Sofa", 1000);
-// prod1.printProductPropertyes();
-
-// const total = prod1.calcProductTaxes(salesTax);
-// console.log(`Total price with taxes ${total}`);
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
-emitter.on("Eventname", (prop) => {
-  console.log("Async event statys is: ", prop);
-});
-emitter.emit("Eventname", "Go!");
 
+emitter.on("evName", (e) => {
+  console.log("My event ", e);
+});
 const log = require("./logger");
-log("logged");
+log("message");
